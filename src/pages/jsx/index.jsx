9bd9e728e60d6c9028760b27e2f4e76029421542
q364067187 <364067187@qq.com>
2021-04-21 17:40:51 +0800

    style: 看看是否运用上了

--- src/pages/jsx/index.jsx
+++ src/pages/jsx/index.jsx
@@ -1,37 +1,41 @@
-import { useState, useEffect } from 'react'
-import Search from 'pages/jsx/search';
-import List from 'pages/jsx/list';
-import { cleanObj } from 'utils/index'
-import qs from 'qs'
+import { useState, useEffect } from "react";
+import Search from "pages/jsx/search";
+import List from "pages/jsx/list";
+import { cleanObj } from "utils/index";
+import qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export default () => {
-    const [param, setParam] = useState({
-        name: '',
-        personId: ''
-    })
-    const [list, setList] = useState([]);
-    const [users, setUsers] = useState([])
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(async () => {
-        const res = await fetch(`${apiUrl}/projects?${qs.stringify(cleanObj(param))}`);
-        if(res.ok){
-            const results = await res.json();
-            setList(results)
-        }
-    }, [param])
+  useEffect(async () => {
+    const res = await fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObj(param))}`
+    );
+    if (res.ok) {
+      const results = await res.json();
+      setList(results);
+    }
+  }, [param]);
 
-    useEffect(async () => {
-        const res = await fetch(`${apiUrl}/users`);
-        if(res.ok){
-            const results = await res.json();
-            setUsers(results)
-        }
-    }, [])
+  useEffect(async () => {
+    const res = await fetch(`${apiUrl}/users`);
+    if (res.ok) {
+      const results = await res.json();
+      setUsers(results);
+    }
+  }, []);
 
-    return <div>
-        <Search users={users} param={param} setParam={setParam} />
-        <List list={list} users={users} />
+  return (
+    <div>
+      <Search users={users} param={param} setParam={setParam} />
+      <List list={list} users={users} />
     </div>
-}
+  );
+};
