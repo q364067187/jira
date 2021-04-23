d161e06479dc8068105d68d638604accff079b02
q364067187 <364067187@qq.com>
2021-04-23 18:51:06 +0800

    build: jira-dev-tools安装

--- src/pages/work-pages/login.tsx
+++ src/pages/work-pages/login.tsx
@@ -1,7 +1,26 @@
 import { FormEvent } from "react";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 const Login = () => {
-  const submitHandle = (event: FormEvent) => {};
+  const postLogin = (param: { username: string; password: string }) => {
+    fetch(`${apiUrl}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (res) => {
+      if (res.ok) {
+      }
+    });
+  };
+
+  const submitHandle = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const username = (event.currentTarget.elements[0] as HTMLFormElement).value;
+    const password = (event.currentTarget.elements[1] as HTMLFormElement).value;
+    postLogin({ username, password });
+  };
 
   return (
     <form onSubmit={submitHandle}>
@@ -12,6 +31,9 @@ const Login = () => {
       <div>
         <label htmlFor="password">密码</label>
         <input type="password" id="password" />
+      </div>
+      <div>
+        <button type="submit">登录</button>
       </div>
     </form>
   );
